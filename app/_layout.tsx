ce3543754ca19f4b04cc1830280221aa5962ecd0
Robert Sinko <sinko.robert.t@gmail.com>
2024-12-20 15:30:16 +0200

    add Fira Sans font, install react-native-circular-progress-indicator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import Homepage from '@/components/Homepage';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 import { ThemeContext } from '@/src/context/ThemeContext';
+import { FiraSans_500Medium } from '@expo-google-fonts/fira-sans';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
@@ -24,6 +25,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Fira: FiraSans_500Medium,
     ...FontAwesome.font,
   });
 
--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,11 +1,23 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import CircularProgress from 'react-native-circular-progress-indicator';
 import { Box } from './ui/box';
-import { Text } from './ui/text';
 
 export default function Timer() {
   return (
-    <Box className="mt-16 h-96 w-96 flex-row items-center justify-center rounded-full bg-pomodoro-main p-2">
-      <Text className="text-8xl">17:59</Text>
+    <Box className="mt-16 h-96 w-96 flex-row items-center justify-center rounded-full bg-pomodoro-main p-2 shadow-2xl shadow-pomodoro-typography/5">
+      <CircularProgress
+        value={75}
+        radius={180}
+        duration={0}
+        progressValueColor={'#ecf0f1'}
+        activeStrokeColor={'#F37070'}
+        inActiveStrokeColor="transparent"
+        maxValue={100}
+        title={'PAUSED'}
+        titleColor={'white'}
+        titleStyle={{ fontSize: 16 }}
+        activeStrokeWidth={12}
+        progressFormatter={() => '15:59'}
+      />
     </Box>
   );
 }
